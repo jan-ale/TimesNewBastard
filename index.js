682f033aeb7cd812bb61b850de2b4d7e4f01b5ff
jan-ale <110728920+jan-ale@users.noreply.github.com>
2022-11-05 12:43:55 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function bastardize(element) {
+    //Yeah, I cannot code. Not that much, anyway
+    text=element.innerText
+    html=""
+    while (text!=""){
+        //i'd scream if i had to debug this
+        html+="<span style='font-family: \"Times New Roman\", Times, serif;'>"+(()=>{reurn=text.slice(0,6);text=text.slice(6);return reurn})()+"</span>"
+        html+="<span style='font-family: Arial, Helvetica, sans-serif;'>"+(()=>{reurn=text.slice(0,1);text=text.slice(1);return reurn})()+"<\span>"
+    }
+    // hope you don't have any angle brackets
+    element.innerHTML=html
+}
