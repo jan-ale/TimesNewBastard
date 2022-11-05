a4a8a8c60c296baef7fd53194e0af847f1cda888
jan-ale <110728920+jan-ale@users.noreply.github.com>
2022-11-05 13:02:50 -0500

    Update index.js
    accidentally used a backslash

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function bastardize(element) {
     while (text!=""){
         //i'd scream if i had to debug this
         html+="<span style='font-family: \"Times New Roman\", Times, serif;'>"+(()=>{reurn=text.slice(0,6);text=text.slice(6);return reurn})()+"</span>"
-        html+="<span style='font-family: Arial, Helvetica, sans-serif;'>"+(()=>{reurn=text.slice(0,1);text=text.slice(1);return reurn})()+"<\span>"
+        html+="<span style='font-family: Arial, Helvetica, sans-serif;'>"+(()=>{reurn=text.slice(0,1);text=text.slice(1);return reurn})()+"</span>"
     }
     // hope you don't have any angle brackets
     element.innerHTML=html
